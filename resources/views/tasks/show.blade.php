a6d9b462599bb4aa0bf3b3e8efa18708d9ed1637
yamato <web.programming.yamato@gmail.com>
2017-08-25 09:13:28 +0000

    add column

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,8 @@
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
     
-    <p>{{ $task->content }}</p>
+    <p>ステータス: {{ $task->status }}</p>
+    <p>メッセージ: {{ $task->content }}</p>
     
     {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $task->id]) !!}
     
